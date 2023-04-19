1beab88dd499f4f20cf88863b4707028f2651047
Gabriel Amigo <gabrielp.amigo@gmail.com>
2023-04-18 21:43:27 -0300

    Initial commit

---
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -0,0 +1,33 @@
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Matricula;
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.repository.MatriculaRepository;
+import me.dio.academia.digital.service.IMatriculaService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatriculaServiceImpl implements IMatriculaService {
+
+    @Override
+    public Matricula create(MatriculaForm form) {
+        return null;
+    }
+
+    @Override
+    public Matricula get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Matricula> getAll(String bairro) {
+            return null;
+        }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
